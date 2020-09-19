b9d8462855233f7c42602f80a7a690e0b61037ae
ElOchi <dantezell78@gmail.com>
2020-09-18 21:33:30 -0400

    add file

---
+++ modulo.c
@@ -0,0 +1,14 @@
+ #include<stdio.h>
+   
+   int main()
+   {
+     int a;
+     int b;
+     printf("Number:");
+     scanf("%d", &a);
+     printf("Number: ");
+     scanf("%d", &b);
+     printf("The result of division is: %d\n", a/b);
+     printf("The result of modulo is: %d\n", a%b);
+  }
+
